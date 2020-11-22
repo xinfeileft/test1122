a723539a5c5465ba669ab2898fbd5a09a3e6236c
xinfei_left <xinfei_left@163.com>
2020-11-22 21:35:55 +0800

    实现多种小功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,23 @@
   <div class="hello">
     <h2>{{message}}</h2>
     <h2>{{$store.state.counter}}</h2>
-      <button @click="add">增加1</button>&nbsp; &nbsp;&nbsp;
-      <button @click="sub">减小1</button> 
-      <h2>{{ $store.getters.more17Stu}}</h2>
+      <button @click="addition">增加1</button>&nbsp; &nbsp;&nbsp;
+      <button @click="subtraction">减小1</button> 
+      <button @click="addCount(5)">+5</button>
+      <button @click="addCount(10)">+10</button>
+
+
+    <h2>数组中大于17的元素：</h2>
+      <h2>{{ $store.getters.more17Arr}}</h2>
+    <h2>年龄大于17的学生：</h2>
+      <h2>{{ $store.getters.more20stu}}</h2>
+    <h2>年龄大于17的学生有几条：</h2> 
+      <h2>{{ $store.getters.more20stuLength}}</h2>
+      <button @click="addStudent">添加学生</button>
+
+<h2> ----------app中info的内容是否是响应式的------------</h2>
+      <h2>{{$store.state.info}}</h2>
+      <button @click="updateInfo">修改姓名信息（将kobo改成coderwhy）</button>
   </div>
 </template>
 
@@ -17,12 +31,35 @@ export default {
     }
   },
   methods:{
-      add(){
-        this.$store.state.counter++
-      },
-      sub(){
-        this.$store.state.counter--
-      },
+      addition(){
+       this.$store.commit('increment')
+     },
+     subtraction(){
+       this.$store.commit('decrement')
+     },
+     addCount(count){
+       this.$store.commit('incrementCount',count)
+     },
+     addStudent(){
+       const stu={id:115,name:'why5',age:30}
+       this.$store.commit('addStu',stu)
+     },
+     updateInfo(){
+       //this.$store.commit('updateNewInfo') //matutions中用
+
+      //  this.$store.dispatch('AupdateNewInfo', {
+      //    message:"我是携带的信息",
+      //    success:()=>{
+      //     console.log('操作完成');
+      // }
+       //})
+
+       this.$store.dispatch('AupdateNewInfo','这是携带的信息')
+       .then(res=>{
+         console.log('完成');
+         console.log(res);
+       })
+     }
        
   },
   props: {
--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,74 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    counter:100,
-    stu: [12,52,14,85,62,110,54]
+    counter: 100,
+    arr: [12, 52, 14, 85, 62, 18,110, 54,20,77],
+    students:[
+      { id: 110, name: "why", age: 18 },
+      { id: 111, name: "why1", age: 17 },
+      { id: 112, name: "why2", age: 22 },
+      { id: 113, name: "why3", age: 11 },
+      { id: 114, name: "why4", age: 21 },
+    ],
+    info: {
+      name: "kobo",
+      age: 40,
+      height: 1.98
+    }
   },
   mutations: {
+    //方法
+    increment(state) {
+      state.counter++
+    },
+    decrement(state) {
+      state.counter--
+    },
+    incrementCount(state, count) {
+      state.counter += count
+    },
+    addStu(state, stu) {
+      state.students.push(stu)
+    },
+    updateNewInfo(state) {
+      state.info.name = "coderwhy"
+      // Vue.set(state.info,'address','洛杉矶')//响应式
+      //delete state.info.age 不是响应式
+    }            //Vue.delete(state.info,'age')//响应式
   },
   actions: {
-       
-  },
-  getters:{
-       more17Stu(state) {
-         return  state.stu.filter(s=>s.age>17);
+    //context：上下文
+    AupdateNewInfo(context, payload) {
+      //  setTimeout(()=>{
+      //      context.commit('updateNewInfo');
+      //      console.log(payload.message);
+      //      payload.success()
+
+      //  },1500)   
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          context.commit('updateNewInfo');
+          console.log(payload);
+          resolve('11111')
+        }, 1000)
+      })
     }
+  },
+  getters: {
+    more17Arr(state) {
+      return state.arr.filter(n => n > 17);
+    },
+    more20stu(state) {
+      return state.students.filter(s => s.age > 20)
+    },
+    more20stuLength(state, getters) {
+      return getters.more20stu.length
+    },
+    moreAgeStu(state) {
+      return function (age) {
+        return state.students.filter(s => s.age >= age)
+      }
+    },
   },
   modules: {
   }
